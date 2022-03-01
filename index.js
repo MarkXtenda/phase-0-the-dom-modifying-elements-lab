80da43299aa01570c5d9f8bc09f5c2e2ca21a8ed
MarkXtenda <mark.i2p.1998@gmail.com>
2022-03-01 22:24:42 +1100

    Final

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+document.querySelector("#main").remove();
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory");
+
+
+const champ = document.createElement("p")
+champ.innerHTML = "YOUR-NAME is the champion"
+
+newHeader.appendChild(champ)
